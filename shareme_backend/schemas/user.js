02cc654dbe28b86de481f924f68185244b5df564
Saif Kamal <kamal.saifkamal534@gmail.com>
2022-07-03 20:25:37 -0700

    Latest Test Commit of branch main of project ShareMe on 4th July,2022 at 9.25am

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'user',
+    tittle: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+    ]
+}
